67828e7aabf1a5597c9624c1ec05e745650d4506
gracymmedeiros <125772512+gracymmedeiros@users.noreply.github.com>
2025-06-20 20:16:04 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from pymongo import MongoClient
+
+# String de conexão MongoDB (substituir pelos seus dados)
+client = MongoClient("mongodb+srv://usuario:senha@cluster0.xxxxx.mongodb.net/?retryWrites=true&w=majority")
+
+# Conectando ao banco e coleção
+db = client["meubanco"]
+colecao = db["clientes"]
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    dados = list(colecao.find({}, {"_id": 0}))  # Ignorando o campo _id
+    return templates.TemplateResponse("tabela.html", {"request": request, "dados": dados})
